b060cc1b36d464f4007cf02a93aa367f496d54e8
Frank Osorio <frankosorio4@gmail.com>
2024-04-23 21:32:08 -0300

    feat(header): creating a header component

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import style from "./header.module.css"
+import { Link } from "react-router-dom"
+
+function Header(){
+
+    return(
+        <div className={style.headerContainer}>
+            <nav className={style.navHeader}>
+                <Link to="/">Home</Link>
+                <Link to="/lista-locais">Lista de locais</Link>
+                <Link to="/cadastro">Cadastro</Link>
+                <Link to="/login">Login</Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
